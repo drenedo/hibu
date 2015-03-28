ae4c5719e63aaa27a9f258a6f1fc395b1fa0919e
drenedo <drenedo@gmail.com>
2015-03-28 15:55:36 +0100

    More params

--- hibu/src/main/java/us/renedo/find/hibu/util/VarPool.java
+++ hibu/src/main/java/us/renedo/find/hibu/util/VarPool.java
@@ -8,7 +8,9 @@ public class VarPool {
 	public static final int WAIT = 20000;
 	
 	//public static final String[] WHAT = {"Restaurante","Farmacia","Abogado","Taller","Talleres","Seguridad","Academia","Seguro","Hotel","Libreria","Boda","Residencia","Gestoria","Gimnasio","Guarderia","Carniceria","Fruteria","Charcuteria","Bar","Ventanas","Puertas","Suelos","Trabajos","Modista","Bodega","Autobuses","Pasteleria","Moda","Optica","Gourmet","Joyeria","Servicios","Construccion","Almacen"};
-	public static final String[] WHAT = {"Reformas","Construccion","Pintura","Fontaneros",
+	
+	
+	/*public static final String[] WHAT = {"Reformas","Construccion","Pintura","Fontaneros",
 	"Albañileria","Electricidad","Carpintería","Más Reformas","Hogar",
 	"Toldos","Jardinerias","Jardineria","Muebles","Baño","Cocina",
 	"Aire","acondicionado","Hogar","Gastronomia","Bares","Pasteleria",
@@ -16,7 +18,51 @@ public class VarPool {
 	"Joyerías","Motor","Talleres","Automovil","Motos","Camiones",
 	"Furgonetas","Salud","Fisioterapia","Farmacias","Dentistas",
 	"Psicologos","Opticas","Limpieza","Centros","Estetica","Veterinario",
-	"Peluqueria","Mudanza","Inmobiliaria","Cerrajeria","Guarderia"};
+	"Peluqueria","Mudanza","Inmobiliaria","Cerrajeria","Guarderia",
+	"Banca","Caja de ahorros","Entidad bancaria","Créditos","Hipotecas",
+	"Belleza y estética","Tatuajes","Cirugía estética","Peluquería",
+	"Coches",
+	"Concesionario","Coches usados","Coches nuevos",
+	"Constructoras","Aire acondicionado","Grúas construcción",
+	"Deportes",
+	"Spa","Fitness","Piscinas",
+	"Despachos",
+	"Financieras","Bufete","Notaría",
+	"Empresas de transportes",
+	"Agencia de transporte","Taxis","Transporte de mercancía",
+	"Enseñanza",
+	"Academias de oposiciones","Colegios públicos",
+	"Hogar",
+	"Pintores","Decoración","Electrodomésticos","Sillas","Reformas",
+	"Hotel",
+	"Apartahotel","Casa rural","Hostal",
+	"Informática",
+	"Electrónica","Portátiles","Ordenadores",
+	"Jardinería",
+	"Flores","Viveros","Jardineros",
+	"Muebles",
+	"Armarios","Muebles de cocina","Muebles de oficina",
+	"Ocio",
+	"Espectáculos","Balnearios","Organización de fiestas",
+	"Pisos",
+	"Agencia inmobiliaria","Mudanzas","Promociones inmobiliarias",
+	"Profesionales",
+	"Cerrajeros","Detectives","Traductor jurado",
+	"Publicidad","Agencias de publicidad","Marketing directo","Rótulos",
+	"Cocina internacional","Restaurante italiano","Restaurante japonés",
+	"Salud","Clínicas","Mutuas","Sanidad",
+	"Seguridad","Alarmas","Cajas fuertes","Sistemas de seguridad",
+	"Seguros","Seguro médico","Seguros de vida","Seguros de coche",
+	"Servicios a empresas","Limpieza","Material de oficina","Ropa de trabajo",
+	"Viajes",};*/
+	
+	//public static final String[] WHAT = {"Agencia","viaje","Turismo","rural","Alquiler","coches"};
+	/*public static final String[] WHAT = {"Morcilla","tabaco","cine","video","movil","relojes","bolsos","maleta","trabajo","ropa",
+										 "juguetes","libros","libreria","procurador","procuradores","lamparas","autorepuestos","repuestos",
+										 "masajista","masajes","mensajeria","transportes","movil","autobuses","mascota","pajareria","vigilancia",
+										 "seguridad","fisioterapia","curandero","herbolario","super","iberico","queso","animales","dulces"};*/
+	
+	public static final String[] WHAT = {"pastas","alimentos","congelados","cerveza","vino","bodega","solar","eolica","renovable","linux","menaje","aluminio","encimera","cocina","fontaneria","bano"};
 
 	
 	public final static String URL_START = "http://www.paginasamarillas.es/search/all-ac/all-ma/all-pr/all-is/all-ci/all-ba/all-pu/all-nc/";
